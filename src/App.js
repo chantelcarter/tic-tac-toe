0aecbad4ec427675f2197806f8ecd81f1fe468f6
Sakaa4Short <klanyenek@gmail.com>
2023-12-13 16:49:45 -0800

    initial framework along with notes for finishing the game structure

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import './App.css'
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+
+  const handleClick = (index) => {
+    if (squares[index] || calculateWinner(squares)) {
+      return
+    }
+    return "❌"
+    // let playerOne = (squares[index]) = "❌"
+    // let playerTwo = "⭕️"
+  
+  }
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square />
+      <Square squares={squares} onClick={() => handleClick(index)} />
     </>
   )
 }
 
 export default App
+
+// Focus on establishing a variable that has to do with the X mark and the O mark so that we can alternate between what needs to happen for the onClick and the handleClick as well as establishing arguments for calculateWinner.
+    // utilizing % (modulo) to figure out which direction for figuring out the winner or the positions for the tictactoe game.
+// handleClick may need an if else conditional tree but it has to be established with some logic for the onClick in square.js to be functional.
+// calculateWinner function needs to be created but only after establishing player 1 & 2 as having X and O marks. 
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Square = (props) => {
   return (
-    <div className="square"></div>
+    <div className="grid">
+      {props.squares.map((value, index) => {
+        return(
+          <main key={index}>
+            <div className="square" onClick={props.handleClick}></div>
+          </main>
+        )
+      })}
+    </div>
   )
 }
 export default Square
+
+// onClick parameters or values would go in here and be accessed via props.
+// This is also the reason why we need handleClick defined due to passing information from onClick through with handleClick
